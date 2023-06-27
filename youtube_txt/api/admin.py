eeedeaf47d80e9bdc4f84444fb8b7f77e602a781
yiningzhang233 <130438302+yiningzhang233@users.noreply.github.com>
2023-06-27 16:00:00 +0900

    database

--- youtube_txt/api/admin.py
+++ youtube_txt/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer, LaterList, Video
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(LaterList)
+admin.site.register(Video)
--- youtube_txt/api/models.py
+++ youtube_txt/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    customer_id = models.IntegerField(primary_key=True)
+    customer_name = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+
+
+class Video(models.Model):
+    video_id = models.TextField(primary_key=True)
+    video_url = models.TextField()
+    video_count = models.IntegerField(default=0)
+    video_title = models.TextField()
+
+    def __str__(self):
+        return self.video_id[:50]
+
+
+class LaterList(models.Model):
+    later_list_id = models.IntegerField(primary_key=True)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    video_id = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+
+   
+
