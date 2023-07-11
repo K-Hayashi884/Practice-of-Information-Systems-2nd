30c67083278805aeba85ed858fe1e3857e4016f2
kazuki3087 <matsumoto-kazuki-25@docomo.ne.jp>
2023-07-11 16:51:37 +0900

    temp

--- youtube_txt/account/urls.py
+++ youtube_txt/account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'account', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('register/', views.create_user),
 ]
--- youtube_txt/account/views.py
+++ youtube_txt/account/views.py
@@ -5,9 +5,33 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserSerializer
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import UserManager
+
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, )
+
+@api_view(['POST'])
+def create_user(request):
+    data = request.data
+
+    email = data['email']
+    username = data['username']
+    password = data['password']
+
+    user = UserManager.create_user(
+        email=email,
+        username=username,
+        password=password,
+        )
+    
+    serializer = UserSerializer(user)
+    
+    return Response(serializer.data)
